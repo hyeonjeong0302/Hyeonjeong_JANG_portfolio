d78303f84bb4d1f972d2c83922cc105d5834c161
Hyeonjeong JANG <wkdguswjd7@gmail.com>
2023-06-25 17:57:05 +0900

    happy

--- js/common.js
+++ js/common.js
@@ -1,31 +1,3 @@
-// this.checkCount(req.cookies.connect_time, res);
-
-// exports.checkCount = (connect_time, res) => {
-//   let today = moment();
-//   if (connect_time) {
-//     if (moment(connect_time).isAfter(today, 'day')) {
-//       res.cookie('connect_time', today.format("YYYY-MM-DD"), {
-//         maxAge: 60 * 60 * 24 * 1000
-//       });
-//       this.connectCount();
-//     } else {
-//       often.log('pass');
-//     }
-//   } else {
-//     res.cookie('connect_time', today.format("YYYY-MM-DD"), {
-//       maxAge: 60 * 60 * 24 * 1000
-//     });
-//     this.connectCount();
-//   }
-
-
-// exports.connectCount = () => {
-//   async function start() {
-// 	//데이터베이스 로직
-//   }
-//   start();
-// }}
-
 //다크모드
 let count = 0;
 const mode = document.getElementById("mode");
